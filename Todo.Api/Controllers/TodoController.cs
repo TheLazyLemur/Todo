5337745873a890a79ada3e38d0620f6ebb85cc08
Dan Rousseau <36530232+TheLazyLemur@users.noreply.github.com>
2021-11-17 22:27:59 +0200

    feat(Api Endpoints): Server and all Api endpoints are now accessible  (#1)
    * feat(Api Endpoints): Server and all Api endpoints are now accessible
    
    All the required endpoints setup and ready to be hooked up to a data
    access layer.

---
+++ Todo.Api/Controllers/TodoController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Todo.Api.Data;
+
+namespace Todo.Api.Controllers;
+
+[ApiController]
+[Route("/todos")]
+public class TodoController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<Todos> ListTodos()
+    {
+        return new Todos()
+        {
+            Items = new List<Data.Todo>()
+        };
+    }
+
+    [HttpPost]
+    public ActionResult<Data.Todo> Add()
+    {
+        return Ok(new Data.Todo());
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult<Data.Todo> Update(string id, Data.Todo newTodoValues)
+    {
+        return Ok(new Data.Todo());
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(string id)
+    {
+        return Ok();
+    }
+}
