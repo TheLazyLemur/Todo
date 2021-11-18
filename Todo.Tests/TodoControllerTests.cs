74abbd9b2220076b67fed64ed59e74418995eee5
Dan Rousseau <36530232+TheLazyLemur@users.noreply.github.com>
2021-11-18 08:40:54 +0200

    feat(Testing): Added more tests (#4)
    Added some more tests and fixed naming of test methods

---
+++ Todo.Tests/TodoControllerTests.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Todo.Api.Controllers;
+using Todo.Api.DataAccess;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Todo.Tests;
+
+public class TodoControllerTests
+{
+    private readonly TodoController _sut;
+    private readonly ITestOutputHelper _testOutputHelper;
+    private readonly Mock<IToDoRepository> _mock = new();
+
+    public TodoControllerTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+        _sut = new TodoController(_mock.Object);
+    }
+
+    [Fact]
+    public async Task AddShouldFail_WhenNameIsImLazy_Return500()
+    {
+        var todo = new Todo.Api.Data.Todo()
+        {
+            Id = "abc123",
+            Name = "I'm lazy",
+            Completed = false
+        };
+
+        _mock.Setup(x => x.Add(todo)).ThrowsAsync(new Exception());
+
+        var response = await _sut.Add(todo);
+
+        var n = response.Result as ObjectResult;
+
+        Assert.NotNull(n);
+        Assert.Equal(500, n.StatusCode);
+    }
+}
